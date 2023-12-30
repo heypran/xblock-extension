27bcc7fda3eb18dff18bc8671ebc32afd9b443c5
heypran <pranjal.baweja@gmail.com>
2023-12-30 13:21:14 +0100

    feat: init

--- src/background.ts
+++ src/background.ts
@@ -3,4 +3,121 @@ function polling() {
   setTimeout(polling, 1000 * 30);
 }
 
-polling();
+// polling();
+let popupWindowId: any = null;
+
+// Handle messages from the content script
+// chrome.runtime.onConnect.addListener((port) => {
+//   if (port.name === "content-script") {
+//     console.log("content-script");
+//     port.onMessage.addListener((msg) => {
+//       console.log("msg", msg);
+//       if (msg.type === "popupOpened") {
+//         // Record the popup window ID
+//         popupWindowId = port.sender?.tab?.id;
+//       }
+//     });
+//   }
+// });
+
+// Establish a connection with the content script
+chrome.runtime.onConnect.addListener((port) => {
+  if (port.name === "content-script") {
+    // Listen for messages from the content script
+    console.log("port.name");
+    port.onMessage.addListener((msg) => {
+      console.log("msg", msg);
+      if (msg.type === "signingRequestDetected") {
+        console.log("signingRequestDetected");
+        // Handle the signing request detection
+        // Open a popup or perform any other action
+        if (popupWindowId) {
+          chrome.windows.update(popupWindowId, { focused: true });
+        } else {
+          // Check if the content script is already injected
+          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            if (
+              !activeTab.url?.startsWith("chrome-extension://") &&
+              activeTab.id
+            ) {
+              // Inject the content script
+              chrome.tabs.executeScript(activeTab.id, {
+                file: "js/content_script.js",
+              });
+            }
+          });
+
+          // Create a new popup window
+          chrome.windows.create(
+            {
+              url: "popup.html", // Replace with the URL of your popup HTML
+              type: "popup",
+              width: 400,
+              height: 300,
+            },
+            (window) => {
+              popupWindowId = window?.id;
+            }
+          );
+        }
+      }
+    });
+  }
+});
+
+// Handle messages from the content script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "signingRequestDetected") {
+    console.log("signingRequestDetected");
+    // Handle the signing request detection
+    // Open a popup or perform any other action
+    if (popupWindowId) {
+      chrome.windows.update(popupWindowId, { focused: true });
+    } else {
+      // Check if the content script is already injected
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0];
+        if (!activeTab.url?.startsWith("chrome-extension://") && activeTab.id) {
+          // Inject the content script
+          chrome.tabs.executeScript(activeTab.id, {
+            file: "js/content_script.js",
+          });
+        }
+      });
+
+      // Create a new popup window
+      chrome.windows.create(
+        {
+          url: "popup.html", // Replace with the URL of your popup HTML
+          type: "popup",
+          width: 400,
+          height: 300,
+        },
+        (window) => {
+          popupWindowId = window?.id;
+        }
+      );
+    }
+  }
+});
+
+// Listen for tab changes
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    // Inject the content script when the tab is fully loaded
+
+    if (
+      tab?.url?.includes("chrome://") ||
+      tab?.url?.includes("chrome-extension://")
+    ) {
+      console.log("can`t run on start page");
+    } else {
+      console.log("chrome.scripting.executeScrip");
+      chrome.scripting.executeScript({
+        target: { tabId, allFrames: true },
+        files: ["js/content_script.js"],
+      });
+    }
+  }
+});
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,71 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+// Function to extract payload from a transaction
+function extractPayload(transaction: any) {
+  // Customize this function based on the structure of your transaction payload
+  console.log("transaction", transaction);
+  return transaction.input;
+}
+
+// Inject a content script to the current tab
+function injectContentScript() {
+  const script = document.createElement("script");
+  script.textContent = `
+    // Capture the send function of the wallet
+    const originalSend = window.ethereum.send;
+    window.ethereum.send = async (payload, callback) => {
+      // Check if the payload is related to signing a transaction
+      if (payload.method === 'eth_sendTransaction') {
+        const transaction = payload.params[0];
+        const extractedPayload = extractPayload(transaction);
+        console.log('Extracted Payload:', extractedPayload);
+        // Add your custom logic here to handle the extracted payload
+        // Send a message to the background script through the established connection
+        port.postMessage({ type: "popupOpened" });
+        chrome.runtime.sendMessage({ type: 'signingRequestDetected', payload: extractedPayload });
+      }
+   
+
+      // Check if the payload is related to signing a message
+      if (payload.method === 'eth_sign') {
+        const signatureRequest = payload.params[1];
+        const extractedPayload = extractPayload(signatureRequest);
+        console.log('Extracted Payload for Signature Request:', extractedPayload);
+        // Add your custom logic here to handle the extracted payload for signature requests
+        // Send a message to the background script through the established connection
+        port.postMessage({ type: "popupOpened" });
+        chrome.runtime.sendMessage({ type: 'signingRequestDetected', payload: extractedPayload });
+      }
+
+      // Call the original send function
+      return originalSend.call(window.ethereum, payload, callback);
+    };
+  `;
+
+  document.documentElement.appendChild(script);
+  script.remove();
+}
+// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+//   if (msg.color) {
+//     console.log("Receive color = " + msg.color);
+//     document.body.style.backgroundColor = msg.color;
+//     sendResponse("Change color to " + msg.color);
+//   } else {
+//     sendResponse("Color message is none.");
+//   }
+// });
+
+// Establish a connection with the background script
+const port = chrome.runtime.connect({ name: "content-script" });
+
+// Listen for messages from the background script
+window.addEventListener("message", (event) => {
+  if (
+    event.source === window &&
+    event.data.type &&
+    event.data.type === "popupOpened"
+  ) {
+    // Forward the message to the background script
+    port.postMessage({ type: "popupOpened" });
   }
 });
+// Wait for the DOM to be ready before injecting the content script
+document.addEventListener("DOMContentLoaded", injectContentScript);
